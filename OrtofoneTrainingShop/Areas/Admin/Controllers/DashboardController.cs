b5306c04a30083017273d044d0029dfddfd455b5
ortofone24 <ortofone24@gmail.com>
2018-12-26 22:18:57 +0100

    Dashboard Admin

---
+++ OrtofoneTrainingShop/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OrtofoneTrainingShop.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
