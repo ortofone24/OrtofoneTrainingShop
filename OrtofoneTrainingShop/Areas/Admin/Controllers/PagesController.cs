20a76e608775ffb7eee99450a63b56ae03fd5f76
ortofone24 <ortofone24@gmail.com>
2018-12-29 10:39:16 +0100

    Admin Pages cz4

--- OrtofoneTrainingShop/Areas/Admin/Controllers/PagesController.cs
+++ OrtofoneTrainingShop/Areas/Admin/Controllers/PagesController.cs
@@ -27,11 +27,65 @@ namespace OrtofoneTrainingShop.Areas.Admin.Controllers
             return View(pagesList);
         }
 
-        // POST: Admin/Views/Pages
+        // GET: Admin/Views/Pages
+        [HttpGet]
         public ActionResult AddPage()
         {
 
             return View();
         }
+
+        // POST: Admin/Views/Pages
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Sprawdzanie model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Database db = new Database())
+            {
+                string slug;
+
+                // Inicjalizacja PageDTO
+                PageDTO dto = new PageDTO();
+
+                
+
+                // gdy niemamy adresu strony to przypisujemy tytuł
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ","-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // zapobiegamy dodaniu takiej samej nazwy strony
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Ten tytuł lub adres strony już istnieje.");
+                    return View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                //zapis DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+
+            return RedirectToAction("AddPage");
+        }
+
     }
 }
