3e27f88cb9e0507ae5eaaa075336ee1847ca9523
ortofone24 <ortofone24@gmail.com>
2018-12-29 05:00:33 +0100

    Admin Pages cz2

--- OrtofoneTrainingShop/Areas/Admin/Controllers/PagesController.cs
+++ OrtofoneTrainingShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using OrtofoneTrainingShop.Models.Data;
+using OrtofoneTrainingShop.Models.ViewModels.Pages;
 
 namespace OrtofoneTrainingShop.Areas.Admin.Controllers
 {
@@ -7,7 +12,19 @@ namespace OrtofoneTrainingShop.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            // Deklaracja listy PageVM
+            List<PageVM> pagesList;
+
+           
+            using (Database db = new Database())
+            {
+                // Inicjalizacja  listy
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+            }
+
+            //zwracamy strony do widoku
+            return View(pagesList);
         }
     }
 }
