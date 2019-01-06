4d75e6508824fd28bb11f83485aa34f382345bd4
ortofone24 <ortofone24@gmail.com>
2019-01-06 23:20:18 +0100

    Admin Shop cz5 Dodawanie kategorii kontroler

--- OrtofoneTrainingShop/Areas/Admin/Controllers/ShopController.cs
+++ OrtofoneTrainingShop/Areas/Admin/Controllers/ShopController.cs
@@ -26,5 +26,38 @@ namespace OrtofoneTrainingShop.Areas.Admin.Controllers
 
             return View(categoryVMList);
         }
+
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //Deklaracja id
+            string id;
+
+            using (Database db = new Database())
+            {
+                // Sprawdzenie czy nazwa kategoria jest unikalna
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "tytulzajety";
+                }
+
+                // Inicjalizacja dto
+                CategoryDTO dto = new CategoryDTO();
+
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 1000;
+
+                // Zapis do bazy
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //Pobieramy id
+                id = dto.Id.ToString();
+            }
+            return id;
+        }
     }
 }
