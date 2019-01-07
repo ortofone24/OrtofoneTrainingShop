18930b58ffdeaeb454cbaf0e2799f5090e220785
ortofone24 <ortofone24@gmail.com>
2019-01-07 02:06:33 +0100

    Admin shop cz8 zmiana nazwy kategorii controller

--- OrtofoneTrainingShop/Areas/Admin/Controllers/ShopController.cs
+++ OrtofoneTrainingShop/Areas/Admin/Controllers/ShopController.cs
@@ -109,5 +109,32 @@ namespace OrtofoneTrainingShop.Areas.Admin.Controllers
 
             return RedirectToAction("Categories");
         }
+
+        // POST Admin/Shop/RenameCategory
+        [HttpPost]
+        public string RenameCategory(string newCatName, int id)
+        {
+            //kontekst
+
+            using (Database db = new Database())
+            {
+                // sprawdzamy czy kategoria jest unikalna
+                if (db.Categories.Any(x => x.Name == newCatName))
+                {
+                    return "tytulzajety";
+                }
+
+                //pobieramy kategorie 
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // edycja kategorii
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+
+                //zapis na bazie
+                db.SaveChanges();
+            }
+            return "Ok";
+        }
     }
 }
