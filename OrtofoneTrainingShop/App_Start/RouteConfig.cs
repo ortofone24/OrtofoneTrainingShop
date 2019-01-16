0335ec8e8b72701ae7bf7f396c115595471ec86a
ortofone24 <ortofone24@gmail.com>
2019-01-17 00:36:18 +0100

    Front Pages cz1 dodanie kontrolera i widoku i tras

--- OrtofoneTrainingShop/App_Start/RouteConfig.cs
+++ OrtofoneTrainingShop/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace OrtofoneTrainingShop
@@ -13,11 +9,20 @@ namespace OrtofoneTrainingShop
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" },
+                new[] { "OrtofoneTrainingShop.Controllers" });
+
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" },
+                new[] { "OrtofoneTrainingShop.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+
+
         }
     }
 }
---
+++ OrtofoneTrainingShop/Controllers/PagesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrtofoneTrainingShop.Models.Data;
+using OrtofoneTrainingShop.Models.ViewModels.Pages;
+
+namespace OrtofoneTrainingShop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{pages}
+        public ActionResult Index(string page = "")
+        {
+            // ustawiamy adres naszej strony
+            if (page == "")
+            {
+                page = "home";
+            }
+            
+            // deklarujemy pageVM i pageDTO
+            PageVM model;
+            PageDTO dto;
+
+            // sprawdzamy czy strona istnieje
+            using (Database db = new Database())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new {page = ""});
+                }
+            }
+
+            // pobieramy pageDTO
+            using (Database db = new Database())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // ustawiamy tytuł naszej strony
+            ViewBag.PageTitle = dto.Title;
+
+            // sprawdzamy czy strona ma pasek boczny z użyciem thernary operator
+            ViewBag.Sidebar = dto.HasSidebar == true ? "Tak" : "Nie";
+
+            
+            // inicjalizujemy model naszego widoku pageVM
+            model = new PageVM(dto);
+
+            // zwracamy widok z modelem pageVM
+            return View(model);
+        }
+    }
+}
