71a5b481d18a090d6a9ab312a4cd77a80d3502b9
ortofone24 <ortofone24@gmail.com>
2019-01-31 14:13:29 +0100

    Rejestracja i logowanie cz9 obsluga rol i sprawdzanie rol w naszej aplikacji

--- OrtofoneTrainingShop/Global.asax.cs
+++ OrtofoneTrainingShop/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using OrtofoneTrainingShop.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -17,5 +19,35 @@ namespace OrtofoneTrainingShop
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_AuthenticateRequest()
+        {
+            // sprawdzamy czy user nie jest nullem
+            if (User == null)
+            {
+                return;
+            }
+
+            // pobieramy nazwę uzytkownika
+            string username = Context.User.Identity.Name;
+
+            // deklaracja tablicy z rolami
+            string[] roles = null;
+
+            using (Database db = new Database())
+            {
+                // pobieramy dlane dla uzytkownika z bazy aby pobrac role
+                UserDTO dto = db.Users.FirstOrDefault(x => x.UserName == username);
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // tworzymy IPrincipal object
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newuserObj = new GenericPrincipal(userIdentity,roles);
+
+            // uaktualniamy kontekst Context.User
+            Context.User = newuserObj;
+        }
+
     }
 }
